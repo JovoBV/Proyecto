7d4c60e443d7d741de38f12d83a9626072913a68
TRUmotion202 <101649860+TRUmotion202@users.noreply.github.com>
2025-08-08 20:15:02 -0500

    Version 1.1.2

--- Calendario.h
+++ Calendario.h
@@ -9,72 +9,52 @@ void mostrarCalendario() {
     time_t rawtime;
     struct tm* timeinfo;
     time(&rawtime);                  
-    timeinfo = localtime(&rawtime);  
-    int mes = timeinfo->tm_mon + 1;   
+    timeinfo = localtime(&rawtime);
+    string mesNombre;  
+    int mes = timeinfo->tm_mon + 1;   // +1 porque tm_mon es 0-11
     int anio = timeinfo->tm_year + 1900; 
-    string months[] = {
-        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
-    };
     int days_in_month[] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
     // Año bisiesto
     if ((anio % 4 == 0 && anio % 100 != 0) || anio % 400 == 0)
         days_in_month[1] = 29;
         
-        switch (mes){
-        case 1:
-            cout << "Calendario del mes: Enero " << anio << endl;
-            break;
-        case 2:
-            cout << "Calendario del mes: Febrero " << anio << endl;
-            break;
-            case 3:
-            cout << "Calendario del mes: Marzo " << anio << endl;
-            break;
-            case 4:
-            cout << "Calendario del mes: Abril " << anio << endl;
-            break;
-            case 5:
-            cout << "Calendario del mes: Mayo " << anio << endl;
-            break;
-            case 6:
-            cout << "Calendario del mes: Enero " << anio << endl;
-            break;
-            case 7:
-            cout << "Calendario del mes: Enero " << anio << endl;
-            break;
-            case 8:
-            cout << "Calendario del mes: Enero " << anio << endl;
-            break;
-            case 9:
-            cout << "Calendario del mes: Enero " << anio << endl;
-            break;
-            case 10:
-            cout << "Calendario del mes: Enero " << anio << endl;
-            break;
-            case 11:
-            cout << "Calendario del mes: Enero " << anio << endl;
-            break;
-            case 12:
-            cout << "Calendario del mes: Enero " << anio << endl;
-        default:
-            cout << "Mes no valido" << endl;
-            break;
-        }
+    switch (mes){
+        case 1: mesNombre = "Enero"; break;
+        case 2: mesNombre = "Febrero"; break;
+        case 3: mesNombre = "Marzo"; break;
+        case 4: mesNombre = "Abril"; break;
+        case 5: mesNombre = "Mayo"; break;       
+        case 6: mesNombre = "Junio"; break;
+        case 7: mesNombre = "Julio"; break;
+        case 8: mesNombre = "Agosto"; break;
+        case 9: mesNombre = "Septiembre"; break;
+        case 10: mesNombre = "Octubre"; break;
+        case 11: mesNombre = "Noviembre"; break;
+        case 12: mesNombre = "Diciembre"; break;
+        default: mesNombre = "Mes no válido"; break;
+    }
 
+    // Obtener el día de la semana del primer día del mes
+    struct tm timeCalendar = {0};
+    timeCalendar.tm_year = anio - 1900;
+    timeCalendar.tm_mon = mes - 1;
+    timeCalendar.tm_mday = 1;
+    mktime(&timeCalendar);
+    int day_of_week = timeCalendar.tm_wday;
 
-        
-        for (int month = 0; month < 12; month++) {
-        cout << "\nCalendario " << months[month] << " " << year << "\n";
-        cout << " D  L  M  MI  J  V  S\n";
-        for (int i = 0; i < day_of_week; i++) cout << "   ";
-        for (int day = 1; day <= days_in_month[month]; day++) {
-            cout << (day < 10 ? "  " : " ") << day;
-            if ((day_of_week + day) % 7 == 0) cout << "\n";
-        }
-        if ((day_of_week + days_in_month[month]) % 7 != 0) cout << "\n";
-        day_of_week = (day_of_week + days_in_month[month]) % 7;
+    cout << "\nCalendario " << mesNombre << " " << anio << "\n";
+    cout << " D  L  M  X  J  V  S\n";  // Orden Domingo a Sábado
+
+    // Espacios para alinear el primer día
+    for (int i = 0; i < day_of_week; i++)
+        cout << "   ";
+
+    // Imprimir días
+    for (int day = 1; day <= days_in_month[mes-1]; day++) {
+        cout << (day < 10 ? " " : "") << day << " ";
+        if ((day_of_week + day) % 7 == 0 || day == days_in_month[mes-1])
+            cout << "\n";
     }
 }
 #endif
